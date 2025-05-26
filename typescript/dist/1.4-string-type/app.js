2dc6c50b846d78e8a82db6f1ff397b3e755fc2b7
Bruno Mendonça <101358061+DevBrunomendonca@users.noreply.github.com>
2025-05-26 10:54:07 -0300

    deleted folder dist

--- typescript/dist/1.4-string-type/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let nameCompleted = "Bruno Mendonça";
-console.log(nameCompleted);
-let companyFunction = "Front-end Developer";
-console.log(companyFunction);
-let nameCompany = "TOTVS";
-let dataEmployee = `Seja bem-vinda ${nameCompleted}! Você será ${companyFunction} 
-    na empresa ${nameCompany}`;
-console.log(dataEmployee);
