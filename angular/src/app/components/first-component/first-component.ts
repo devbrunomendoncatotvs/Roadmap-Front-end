42e88f92ef2242a906c322b0a494afd600710fd8
Bruno Mendonça <bruno.belo@totvs.com.br>
2025-05-29 11:21:56 -0300

    add branch 3_interpolacao

--- angular/src/app/components/first-component/first-component.ts
+++ angular/src/app/components/first-component/first-component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-first-component',
   imports: [],
   templateUrl: './first-component.html',
-  styleUrl: './first-component.css'
+  styleUrl: './first-component.css',
 })
 export class FirstComponent {
+  name: string = 'Bruno Mendonça';
+  age: number = 27;
+  job: string = 'Front-end Developer';
+  hobbies: string[] = ['Treinar', 'Estudar', 'Codar'];
+  car = {
+    name: 'Civic',
+    year: 2013,
+  };
 
+  constructor() {}
 }
