017b1b4b534f043759e5d3d321df7b8d6e1c250e
Bruno Mendonça <bruno.belo@totvs.com.br>
2025-05-29 10:52:38 -0300

    add component first-component

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './components/first-component/first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FirstComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular';
